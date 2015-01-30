6aff062c7c160917267d584c7e2dcd3f18044d60
Brandon Hutchens <bhhutchens@gmail.com>
2015-01-30 15:27:53 -0800

    added vegan and total calories methods

--- burrito_gen/app/models/burrito.rb
+++ burrito_gen/app/models/burrito.rb
@@ -2,4 +2,22 @@
 class Burrito < ActiveRecord::Base
   has_many :combinations
   has_many :ingredients, through: :combinations
+
+  def total_calories
+    total = 0
+    self.ingredients.each do |ingredient|
+      total += ingredient.calories
+    end
+    return total
+  end
+
+  def vegan?
+    self.ingredients.each do |ingredient|
+      if ingredient.vegan == false
+        return false
+      end
+    end
+    return true
+  end
+
 end
